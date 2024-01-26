8947c94ec7dc2444711d19c11a4e607f987915b2
Lincoln-Smith <144719300+Lincoln-Smith@users.noreply.github.com>
2024-01-26 17:59:31 -0500

    frankly i give up, we're going for functionality, not deisgn

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+function updateInfo(content) {
+    document.getElementById("info").innerText = content;
+}
+
 function change1789() {
-    document.getElementById('info').innerText = "1957- The Little Rock Nine begin their first full day of classes at Central High School in Little Rock, Arkansas.     "
+    updateInfo("1789- U.S. Congress passes the Bill of Rights, the first 10 amendments to the U.S. Constitution. The rights the amendments establish include freedom of speech and the press, the right to bear arms and numerous legal rights of those accused of crimes, including the right to a trial by an impartial jury, protection from cruel and unusual punishments and protection against self-incrimination.");
 }
+
 function change1957() {
-    
+    updateInfo("1957- The Little Rock Nine begin their first full day of classes at Central High School in Little Rock, Arkansas.");
 }
+
 function change1981() {
-    
+    updateInfo("1981- Sandra Day O'Connor, the first female U.S. Supreme Court Justice, is sworn in.");
 }
+
 function change1992() {
-   
+    updateInfo("1992 NASA launches the Mars Observer. The robotic space probe's main goal was to study Mars. Almost a year later all communications with it were lost.");
 }
+
 function change2005() {
-    
+    updateInfo("2005 - IRA officially disarms.");
 }
+
