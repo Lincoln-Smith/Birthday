4a7eab645d53db3b151da5e70c925f8452a88310
Lincoln-Smith <Lincolnsmith628@gmail.com>
2024-01-26 15:00:39 -0500

    Im gonna kill myself

---
+++ script.js
@@ -0,0 +1,15 @@
+function change1789() {
+    document.getElementById('info').innerText = "1957- The Little Rock Nine begin their first full day of classes at Central High School in Little Rock, Arkansas.     "
+}
+function change1957() {
+    
+}
+function change1981() {
+    
+}
+function change1992() {
+   
+}
+function change2005() {
+    
+}
