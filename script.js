e2ad67af494bfed58c7c42005139d01aeb422731
Lincoln-Smith <Lincolnsmith628@gmail.com>
2024-01-31 13:19:53 -0500

    just needs a couple small things!!!!!

--- script.js
+++ script.js
@@ -2,32 +2,32 @@
 // Change for the year 1789
 function change1789() {
    document.getElementById("info").innerText = "1789- U.S. Congress passes the Bill of Rights, the first 10 amendments to the U.S. Constitution. The rights the amendments establish include freedom of speech and the press, the right to bear arms and numerous legal rights of those accused of crimes, including the right to a trial by an impartial jury, protection from cruel and unusual punishments and protection against self-incrimination."
-   document.getElementById("placeholderImage").src = "imgs/oldwhiteguys.png";
+   document.getElementById("placeholderImage").src = "imgs/racism3.png";
 }
 
 // Change for the year 1957
 function change1957() {
     document.getElementById("info").innerText = "1957- The Little Rock Nine begin their first full day of classes at Central High School in Little Rock, Arkansas."
-    document.getElementById("placeholderImage").src = "imgs/civilrights.png";
+    document.getElementById("placeholderImage").src = "imgs/racism2.png";
 }
 
 // Change for the year 1981
 function change1981() {
     document.getElementById("info").innerText =  "1981- Sandra Day O'Connor, the first female U.S. Supreme Court Justice, is sworn in."
-    document.getElementById("placeholderImage").src = "imgs/oconnor.png";
+    document.getElementById("placeholderImage").src = "imgs/oconnor2.png";
 }
 
 // Change for the year 1992
 function change1992() {
 
     document.getElementById("info").innerText = "1992 NASA launches the Mars Observer. The robotic space probe's main goal was to study Mars. Almost a year later all communications with it were lost."
-    document.getElementById("placeholderImage").src = "imgs/billofrights.jpg";
+    document.getElementById("placeholderImage").src = "imgs/mars2.png";
 }
 
 // Change for the year 2005
 function change2005() {
 
     document.getElementById("info").innerText =   "2005 - IRA officially disarms."
-    document.getElementById("placeholderImage").src = "imgs/billofrights.jpg";
+    document.getElementById("placeholderImage").src = "imgs/kid.png";
 
 }
